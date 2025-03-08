ff7fba8f8717411dfdb65b1d045b96b1fa6bf5ac
ZhangTerrence <terrencezhang77@gmail.com>
2025-03-08 14:05:16 -0500

    feat: added nuxt ui

---
+++ components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="flex justify-between p-4">
+    <ULink to="/">TableShare</ULink>
+    <div class="flex gap-x-2">
+      <ColorModeButton />
+      <UButton to="/sign-in">Sign In</UButton>
+      <UButton to="/sign-up">Sign Up</UButton>
+    </div>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue";
+</script>
+
+<template>
+  <div class="w-screen h-screen">
+    <Navbar />
+    <slot />
+  </div>
+</template>
