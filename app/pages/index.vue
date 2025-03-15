0ce172779df501a403c1d194eea97e7cb9a6b34c
ZhangTerrence <terrencezhang77@gmail.com>
2025-03-15 17:17:55 -0400

    chore: added prettier

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="flex flex-col items-center justify-center gap-4 h-screen">
-    <h1 class="font-bold text-2xl text-(--ui-primary)">
-      Nuxt UI v3
-    </h1>
+  <div class="flex h-screen flex-col items-center justify-center gap-4">
+    <h1 class="text-2xl font-bold text-(--ui-primary)">Nuxt UI v3</h1>
 
     <div class="flex items-center gap-2">
       <UButton
